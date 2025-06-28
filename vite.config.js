ddecc05fa64eec5eb800691f5d38034b2dabc613
Mahdi BagheriFard <mahdibagherifard100@gmail.com>
2025-06-28 16:50:43 +0330

    توضیح کوتاه دربارهی تغییرات

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/Tarkhineh/',   build: {
+    outDir: 'docs'
+  },
   plugins: [react()],
 })
