f8263afe22784a86324d249f18ffb414676ee527
Mahdi BagheriFard <mahdibagherifard100@gmail.com>
2025-06-28 16:52:50 +0330

    final build setup for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/Tarkhineh/',   build: {
+  base: '/Tarkhineh/',   build: {
     outDir: 'docs'
   },
   plugins: [react()],
